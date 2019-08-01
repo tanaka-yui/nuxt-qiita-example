0b93e9c49126ef520a144aaad0e85edc24ade285
tanaka yui <tanaka_yui@cyberagent.co.jp>
2019-08-01 22:47:46 +0900

    add page

---
+++ pages/list/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="list">
+    <li v-for="item in data" :key="item.id" class="listItem">
+      {{ item.title }}
+    </li>
+  </ul>
+</template>
+
+<style>
+.list {
+  padding: 0;
+}
+
+.listItem {
+  list-style: none;
+  border-bottom: 1px solid #999;
+  padding: 1rem 16px;
+  box-sizing: border-box;
+}
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class List extends Vue {
+  async asyncData(context) {
+    const res = await context.$axios.get('https://qiita.com/api/v2/items', {
+      headers: {
+        Authorization: 'Bearer xxxxxxxx'
+      }
+    })
+    return {
+      data: res.data
+    }
+  }
+}
+</script>
---
+++ server/index.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+// ここに色々追加していく
+
+module.exports = app
